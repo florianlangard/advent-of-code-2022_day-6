6e0e309a795745d36dcfa2e93a4b6c04f70ad3d9
florianlangard <florian.langard@gmail.com>
2022-12-10 17:13:34 +0100

    part one solved

--- advent.php
+++ advent.php
@@ -1,5 +1,27 @@
 <?php
 
 $raw = file_get_contents('data.txt');
-$data = str_replace("\n\n"," ",$raw);
-$data = explode(" ", $data);
+// $data = str_replace("\n\n"," ",$raw);
+// $data = str_split($raw, 1);
+var_dump($raw);
+
+function partOne($data) {
+
+    $answer = "";
+
+    for ($i=0; $i < strlen($data)-3; $i++) { 
+        
+        $buffer = substr($data, $i, 4);
+    
+        $haystack = str_split($buffer, 1);
+    
+        $filteredHaystack = array_unique($haystack);
+        if (count($filteredHaystack) === count($haystack)) {
+            $answer = $i + 4;
+            break;
+        }
+    }
+    echo $answer;
+}
+
+partOne($raw);
